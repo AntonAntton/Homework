1f45881a3bb8ea3b3925d112354b608ed1d68206
AntonAntton <ivanko.anton1408@gmail.com>
2024-12-10 18:34:35 -0400

    Add files via upload

---
+++ Homework 3.2..py
@@ -0,0 +1,14 @@
+my_list = []
+n = int(input("Amount of numbers: "))
+for i in range(n):
+    y = int(input(f"Enter numbers {i + 1}: "))
+    my_list.append(y)
+print(my_list)
+last_element = my_list.pop()
+my_list.insert(0, last_element)
+print(my_list)
+
+
+
+
+
