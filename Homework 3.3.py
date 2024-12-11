c862a3b2876d9c8cbaec71ca8cb0cd0c5c1b02b1
AntonAntton <ivanko.anton1408@gmail.com>
2024-12-10 20:23:34 -0400

    Add files via upload

---
+++ Homework 3.3.py
@@ -0,0 +1,28 @@
+my_list_1 =[1, 2, 3, 4, 5, 6]
+mid = len(my_list_1) // 2
+list_1 = my_list_1[:mid]
+list_2 = my_list_1[mid:]
+print(list_1, list_2)
+my_list_2 =[1, 2, 3]
+mid = len(my_list_2) // 2
+list_3 = my_list_2[:mid + 1]
+list_4 = my_list_2[mid + 1:]
+print(list_3, list_4)
+my_list_3 =[1, 2, 3, 4, 5]
+mid = len(my_list_3) // 2
+list_5 = my_list_3[:mid + 1]
+list_6 = my_list_3[mid + 1:]
+print(list_5, list_6)
+my_list_4 =[1]
+mid = len(my_list_4) // 2
+list_7 = my_list_4[:mid + 1]
+list_8 = my_list_4[mid + 1:]
+print(list_7, list_8)
+my_list_5 =[]
+mid = len(my_list_5) // 2
+list_9 = my_list_5[:mid + 1]
+list_10 = my_list_5[mid + 1:]
+print(list_9, list_10)
+
+
+
