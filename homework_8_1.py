10d3ca65e3cdd98f69a112b4c2ae8d3bfde4e0db
AntonAntton <ivanko.anton1408@gmail.com>
2024-12-30 18:11:33 -0400

    homework_8_1.py

--- homework_8_1.py
+++ homework_8_1.py
@@ -4,7 +4,7 @@ def add_one(my_numbers):
     return list(map(int, str(number)))
 
 
-my_numbers = list(map(int, input("Enter your numbers(e.g. 1,2,3..): ").split(",")))
+my_numbers = list(input("Enter your numbers(e.g. 1,2,3..): ").split(","))
 print(my_numbers)
 result = add_one(my_numbers)
 print("Result:", result)
