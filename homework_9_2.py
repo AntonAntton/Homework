502db20050e42e4d3bc21bf628d89c577c868838
AntonAntton <ivanko.anton1408@gmail.com>
2025-01-08 12:15:47 -0400

    Update homework_9_2.py

--- homework_9_2.py
+++ homework_9_2.py
@@ -2,9 +2,12 @@ def difference():
     # Запит від користувача
     my_numbers = input("Enter numbers: ").split(",")
 
-    # Перетворюємо запит на ліст з цілими числами
-    my_numbers = [int(num) for num in my_numbers]
+    # Перетворюємо запит на ліст з числами (int або float)
+    def parse_number(num):
+        return int(num) if num.isdigit() else float(num)
 
+    my_numbers = [parse_number(num for num in my_numbers]
+    
     print("User numbers:", my_numbers)
 
     # Знаходимо максимальне і мінімальне значення
