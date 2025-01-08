95743b49004b7c6e931c98715a25dbdfa48ed40d
AntonAntton <ivanko.anton1408@gmail.com>
2025-01-08 12:57:20 -0400

    Update homework_10_2.py

--- homework_10_2.py
+++ homework_10_2.py
@@ -17,7 +17,7 @@ def first_word():
 
     # Розділяємо текст на окремі слова, якщо в текст є пробіли, чи слова починаються з заглавної літери.
     if " " in my_text:
-        words = my_text.split()  # Split by spaces
+        words = my_text.split() 
     else:
         words = re.findall(r'[A-Z][^A-Z]*', my_text)
         if not words:
