32d887a7c46fa39f603b96a8a8724db546c8c1ba
AntonAntton <ivanko.anton1408@gmail.com>
2025-01-09 21:47:00 -0400

    Update homework_10_3.py

--- homework_10_3.py
+++ homework_10_3.py
@@ -1,8 +1,16 @@
-def is_even(digit):
-    """Функція перевіряє чи число парне, чи непарне і повертає True чи False"""
-    return digit % 2 == 0
+def is_even(digit: int) -> bool:
+    """
+    Функція перевіряє чи число парне, чи непарне і повертає True чи False.
+
+    Параметри:
+    digit (int): Ціле число для перевірки.
+
+    Повертає:
+    bool: True, якщо число парне; False, якщо непарне.
+    """
+    return True if digit % 2 == 0 else False
+
 
 # Перевіряємо функцію
 print(is_even(4))
 print(is_even(5))
-print(is_even(0))
